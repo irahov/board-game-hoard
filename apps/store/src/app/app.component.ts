f92ffa21a8e8de417afd284f9fe0f6f0daa5383f
Ivo Rahov <irahov@vmware.com>
2023-04-10 13:50:39 +0300

    Lab 6 showing details using routing

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -4,13 +4,14 @@ import {MatCardModule} from "@angular/material/card";
 import {CommonModule} from "@angular/common";
 import {StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
 import {formatRating} from "@bg-hoard/store/util-formatters";
+import {RouterModule} from "@angular/router";
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MatCardModule, CommonModule, StoreUiSharedModule]
+  imports: [MatCardModule, CommonModule, StoreUiSharedModule, RouterModule]
 })
 export class AppComponent {
   title = 'Board Game Hoard';
