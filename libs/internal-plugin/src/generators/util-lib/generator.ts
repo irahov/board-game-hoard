72cecde3d315bc56e2a874566d848e2005266316
Ivo Rahov <irahov@vmware.com>
2023-04-10 17:11:31 +0300

    Lab13 adding autotags to the generator

--- libs/internal-plugin/src/generators/util-lib/generator.ts
+++ libs/internal-plugin/src/generators/util-lib/generator.ts
@@ -7,6 +7,7 @@ export default async function (tree: Tree, schema: UtilLibGeneratorSchema) {
   await libraryGenerator(tree, {
     name: `util-${schema.name}`,
     directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`,
   });
   await formatFiles(tree);
   return () => {
