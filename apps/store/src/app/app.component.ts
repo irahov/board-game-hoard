6d347d9c65e31685ad9c845b3fbd958aa61c5d93
Ivo Rahov <irahov@vmware.com>
2023-04-10 12:08:22 +0300

    Lab4 create shared lab and header component and use it
    npx nx generate @nrwl/angular:component header --export --project=store-ui-shared

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { getAllGames } from '../fake-api';
 import {MatCardModule} from "@angular/material/card";
 import {CommonModule} from "@angular/common";
+import {StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
 
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MatCardModule, CommonModule]
+  imports: [MatCardModule, CommonModule, StoreUiSharedModule]
 })
 export class AppComponent {
   title = 'Board Game Hoard';
